7c005f6fc604cd5fc29ae05edc3cc0ad743e4218
Nicolas Filzi <nfilzi.webservices@gmail.com>
2020-06-06 17:08:53 +0200

    Move rental amount computation logic in Movie, closer to potential movie type changes, which would affect the logic later

--- 01-introduction/models/movie.rb
+++ 01-introduction/models/movie.rb
@@ -3,10 +3,43 @@ class Movie
   NEW_RELEASE = 1
   CHILDRENS   = 2
 
+  BASE_FREQUENT_RENTER_POINTS       = 1
+  BASE_BONUS_FREQUENT_RENTER_POINTS = 1
+
   attr_reader   :title
   attr_accessor :price_code
 
   def initialize(title, price_code)
     @title, @price_code = title, price_code
+  end
+
+  def amount_due_for(days_rented)
+    amount = 0
+    case price_code
+    when Movie::REGULAR
+      amount += 2
+      amount += (days_rented - 2) * 1.5 if days_rented > 2
+
+    when Movie::NEW_RELEASE
+      amount += days_rented * 3
+
+    when Movie::CHILDRENS
+      amount += 1.5
+      amount += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+
+    amount
+  end
+
+  def total_frequent_renter_points(days_rented)
+    BASE_FREQUENT_RENTER_POINTS + bonus_frequent_renter_points(days_rented)
+  end
+
+  def bonus_frequent_renter_points(days_rented)
+    if price_code == Movie::NEW_RELEASE && days_rented > 1
+      BASE_BONUS_FREQUENT_RENTER_POINTS
+    else
+      0
+    end
   end
 end
--- 01-introduction/models/rental.rb
+++ 01-introduction/models/rental.rb
@@ -1,42 +1,15 @@
 class Rental
   attr_reader :movie, :days_rented
 
-  BASE_FREQUENT_RENTER_POINTS       = 1
-  BASE_BONUS_FREQUENT_RENTER_POINTS = 1
-
   def initialize(movie, days_rented)
     @movie, @days_rented = movie, days_rented
   end
 
   def amount_due
-    @amount_due ||= begin
-      amount = 0
-      case movie.price_code
-      when Movie::REGULAR
-        amount += 2
-        amount += (days_rented - 2) * 1.5 if days_rented > 2
-
-      when Movie::NEW_RELEASE
-        amount += days_rented * 3
-
-      when Movie::CHILDRENS
-        amount += 1.5
-        amount += (days_rented - 3) * 1.5 if days_rented > 3
-      end
-
-      amount
-    end
+    @amount_due ||= movie.amount_due_for(days_rented)
   end
 
   def total_frequent_renter_points
-    BASE_FREQUENT_RENTER_POINTS + bonus_frequent_renter_points
-  end
-
-  def bonus_frequent_renter_points
-    if movie.price_code == Movie::NEW_RELEASE && days_rented > 1
-      BASE_BONUS_FREQUENT_RENTER_POINTS
-    else
-      0
-    end
+    @total_frequent_renter_points ||= movie.total_frequent_renter_points(days_rented)
   end
 end
