6dab33da426f24c2912734b0d9dd6d3a1cbc44e6
Nicolas Filzi <nfilzi.webservices@gmail.com>
2020-06-07 22:59:01 +0200

    Refactor to enable any type of new range based discounts to be added arbitrarily

--- 06-composing-methods/04-replace-temp-with-query/order/item_line.rb
+++ 06-composing-methods/04-replace-temp-with-query/order/item_line.rb
@@ -1,5 +1,10 @@
 class Order
   class ItemLine
+    DISCOUNTS = [
+      { price_range: (1000..1999), rate: 0.95 },
+      { price_range: (1..999),     rate: 0.98 }
+    ]
+
     def initialize(quantity, item_price)
       @quantity, @item_price = quantity, item_price
     end
@@ -13,7 +18,7 @@ class Order
     end
 
     def discount_factor
-      @discount_factor ||= base_price > 1000 ? 0.95 : 0.98
+      @discount_factor ||= DISCOUNTS.find { |price_range: , rate:| price_range.include? base_price }[:rate]
     end
   end
 end
